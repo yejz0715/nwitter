cce194d4ba1e13f68cb72d5e2ab034e91998b507
yejz0715 <yejz0715@gmail.com>
2023-05-23 23:14:00 +0900

    firebase 설정

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,18 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyALpL0uV-70Kdo4mlpUTe4VYdF2LI1m744",
-  authDomain: "nwitter-8bf2f.firebaseapp.com",
-  projectId: "nwitter-8bf2f",
-  storageBucket: "nwitter-8bf2f.appspot.com",
-  messagingSenderId: "297278238891",
-  appId: "1:297278238891:web:34d81d2cff183e078347ee",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
 
 export default app;
